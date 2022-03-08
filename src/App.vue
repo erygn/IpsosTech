30525668fb2ea529a74bf05b85325b764a405afb
erygn <etienne.reygner@gmail.com>
2022-03-08 23:50:39 +0100

    Update style

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="bg-gray-100 min-h-screen w-full text-center flex justify-center">
-    <div class="px-4 pt-8 max-w-7xl">
-      <h1 class="text-center text-6xl font-bold mb-4">IpsosTech</h1>
-      <p class="mb-8">Pour suivre vos listes favorites</p>
+  <div class="min-h-screen w-full text-center" style="background-image: url('https://controller.genesis-mc.fr/uploads/5172658.jpg'); background-position: center; background-size: cover">
+    <nav class="bg-white z-50 shadow-2xl flex px-4 md:px-32 py-2 fixed top-0 items-center w-full justify-between">
+      <span class="font-bold">IpsosTech</span>
+      <a href="https://bdepolytechgrenoble.fr/" target="_blank"><button class="px-4 py-2 text-sm text-white bg-blue-900 rounded-full">Polytech</button></a>
+    </nav>
 
-      <h2 class="font-bold text-xl">Votez pour votre liste</h2>
-      <div class="grid grid-cols-3 gap-4">
-        <div class="p-4" v-for="(item, index) in list" :key="index">
-          <div class="aspect-square rounded-full mb-2" :style="'background-image: url(\'' + item.img + '\'); background-position: center; background-size: contain'"></div>
-          <h2 class="font-bold h-12 mb-2">{{ item.title }}</h2>
+    <div class="px-4 pt-48">
+      <h1 class="text-center text-white text-6xl font-bold mb-4">IpsosTech</h1>
+      <p class="mb-24 text-white">Pour suivre vos listes favorites</p>
 
-          <button @click="up(index)" class="w-full rounded text-white p-1" :style="'background-color:' + item.color">+</button>
-          <div class="font-bold p-1">{{ item.value }}</div>
-          <button @click="down(index)" class="w-full rounded text-white p-1" :style="'background-color:' + item.color">-</button>
+      <div class="w-full flex justify-center">
+        <div class="max-w-6xl w-full md:mx-32 md:grid md:grid-cols-3 md:gap-4">
+          <div class="py-2 md:px-2 skew-y-3" v-for="(item, index) in list" :key="index">
+            <div class="flex items-center md:block p-4 md:p-8 rounded-xl drop-shadow-2xl" :style="'color: '+ item.colorText +'; background-color: ' + item.color">
+              <div class="aspect-square cursor-pointer w-32 md:w-full rounded-full mb-2" :style="'background-image: url(\'' + item.img + '\'); background-position: center; background-size: contain'"></div>
+              <!-- <h2 class="font-bold h-12 mb-2 md:text-4xl">{{ item.title }}</h2> -->
+
+              <div class="flex-1 px-4 md:px-0">
+                <button @click="up(index)" class="w-full font-bold text-xl rounded bg-white p-1 pb-2" :style="'color: ' + item.color">+</button>
+                <div class="font-bold p-1 text-xl py-2">{{ item.value }}</div>
+                <button @click="down(index)" class="w-full font-bold text-xl rounded bg-white p-1 pb-2" :style="'color: ' + item.color">-</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="mt-4">
+      <div class="mt-4 text-white mt-16 pb-16">
         <p>Site mis en place avec l'aide de <a class="underline text-blue-600 font-bold" href="https://rakura.fr" target="_blank">Rakura</a>, site d'escape game en ligne</p>
       </div>
     </div>
